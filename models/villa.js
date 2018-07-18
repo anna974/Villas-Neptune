169d5a0360dc27e7dab4cc6cc55d816f70d04a7a
anna974 <annapiaraly@gmail.com>
2018-07-18 06:48:59 +0400

    first commit

---
+++ models/villa.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+var VillaSchema = new mongoose.Schema({
+    photo_pers: String,
+    photo_rdc: String,
+    photo_etage: String,
+    nb_pieces: Number,
+    nb_chambres: Number,
+    prix: Number,
+    shob:Number,
+    shon:Number,
+    surface_cuisine:Number,
+    surface_sejour:Number,
+    surface_rangement:Number,
+    surface_cellier:Number,
+    surface_wc:Number,
+    surface_suiteparentale:Number,
+    surface_chambre1:Number,
+    surface_chambre2:Number,
+    surface_chambre3:Number,
+    surface_salledebains:Number,
+    surface_habitable_rdc:Number,
+    surface_habitable_etage:Number,
+    surface_varangue:Number,
+});
+
+module.exports = mongoose.model("villa", VillaSchema);
