54727c9bb9de96b3a502205773c52d799271ffb5
anna974 <annapiaraly@gmail.com>
2018-07-18 08:37:41 +0400

    maj

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var villa = require("../models/villa");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Villas Neptune' });
 });
+
+
+router.get('/catalogue', function (req, res) {
+  villa.find({}).exec(function (err, villa) {
+    if (err) {
+      console.log('Error : ', err);
+    } else {
+      console.log("->", villa);
+      res.render("catalogue", { villa: villa });
+    }
+  });
+})
+
 
 module.exports = router;
